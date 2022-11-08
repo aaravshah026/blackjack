d7317a74e7860b9253f82cc7dba7be55db5ac9ed
aaravshah026 <99697973+aaravshah026@users.noreply.github.com>
2022-11-07 20:27:06 -0500

    Add files via upload
    Card.java (v1.0) - class representing Card objects to be used in blackjack
    CardSuit.java (v1.0) - enum representing the suit of a Card object
    CardValue.java(v1.0) - enum representing the value of a Card object

---
+++ Card.java
@@ -0,0 +1,50 @@
+/**
+ * Class Card: describes Card objects in a game of blackjack.
+ * @author aaravshah026
+ * @version 1.0
+ */
+public class Card {
+
+    private CardValue value;
+    private CardSuit suit;
+
+    /**
+     * Constructor for Card objects.
+     * @param value CardValue representing the value of the Card
+     * @param suit CardSuit representing the suit of the Card
+     */
+    public Card(CardValue value, CardSuit suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("The %s of %s", this.value.toString().toLowerCase(), this.suit.toString().toLowerCase());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+        if ((this.getSuit() != ((Card) o).getSuit()) || (this.getValue() != ((Card) o).getValue()))
+            return false;
+        return true;
+    }
+
+    /**
+     * Getter for the suit of the Card.
+     * @return CardSuit representing the suit of the Card
+     */
+    public CardSuit getSuit() {
+        return this.suit;
+    }
+
+    /**
+     * Getter for the value fo the Card.
+     * @return CardValue representing the value of the Card
+     */
+    public CardValue getValue() {
+        return this.value;
+    }
+}
