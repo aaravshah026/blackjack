5f7ad12187f3734310baaab9e7c2250ccf23d99f
Aarav Shah <99697973+aaravshah026@users.noreply.github.com>
2023-04-25 23:28:19 -0400

    Update Blackjack.java
    Fixed bugs related to gameplay.

--- code/Blackjack.java
+++ code/Blackjack.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 /**
  * Blackjack.java
- * Class containing all the methods needed to play a game of standard Blackjack, without splitting, doubling down, or insurance.
+ * Class containing all the methods needed to play a game of standard Blackjack, without splitting,
+ * doubling down, or insurance.
  *
  * @author Aarav Shah
- * @version 1.0
+ * @version 1.1
  */
 public class Blackjack {
 
     Deck deck;
     BlackjackHand userHand;
     BlackjackHand dealerHand;
+    Scanner in = new Scanner(System.in);
 
     /**
      * Constructor method for a Blackjack table.
@@ -26,7 +28,7 @@ public class Blackjack {
      * Method to draw cards for the user, with the user's permission.
      */
     public void DrawUser() {
-        Scanner in = new Scanner(System.in);
+        System.out.println();
         System.out.print("Would you like to draw a card? Y/N: ");
         String userInput = in.nextLine();
         while (!userInput.equals("Y") && !userInput.equals("N")) {
@@ -56,14 +58,13 @@ public class Blackjack {
         // the user's hand value is below 21
 
         System.out.println();
-        in.close();
     }
 
     /**
      * Method for dealing out cards to the dealer, after the user is done adding cards.
      */
     public void DrawDealer() {
-        while (dealerHand.getHandValue() < userHand.getHandValue() &&
+        while (dealerHand.getHandValue() <= userHand.getHandValue() &&
         dealerHand.getHandValue() < 21) {
             Card c = deck.removeCard();
             dealerHand.add(c);
@@ -73,6 +74,7 @@ public class Blackjack {
         // the user hand, or the dealer hand is over 21.
 
         System.out.println("The dealer's final hand value is " + dealerHand.getHandValue());
+        System.out.println();
     }
 
     /**
@@ -80,15 +82,17 @@ public class Blackjack {
      * @return boolean representing whether the user wants to play again or not
      */
     public boolean calculateResult() {
-        String userInput = "A";
-        Scanner in = new Scanner(System.in);
-        if (userHand.getHandValue() <= 21 && userHand.getHandValue() > dealerHand.getHandValue()) {
-            System.out.println("You win! Congratulations! Would you like to play again? Y/N: ");
+        String userInput;
+        if (userHand.getHandValue() <= 21 && (userHand.getHandValue() > dealerHand.getHandValue() ||
+                dealerHand.getHandValue() > 21)) {
+            System.out.print("You win! Congratulations! Would you like to play again? Y/N: ");
+            userInput = in.nextLine();
         } else if (userHand.getHandValue() == dealerHand.getHandValue()) {
             System.out.println("You draw with the dealer! Time to redraw cards...");
             userInput = "Y";
         } else {
-            System.out.println("You lose! What a shame. Would you like to play again? Y/N: ");
+            System.out.print("You lose! What a shame. Would you like to play again? Y/N: ");
+            userInput = in.nextLine();
         }
         while (!userInput.equals("Y") && !userInput.equals("N")) {
             System.out.print("Please type Y for yes, N for no: ");
@@ -104,10 +108,29 @@ public class Blackjack {
     public boolean playOneBlackjackGame() {
         System.out.println("Welcome to the Blackjack table! Get ready to play!");
         DrawUser();
+        boolean result;
         if (userHand.getHandValue() >= 21) {
-            return calculateResult();
+            result = calculateResult();
+        } else if (userHand.getHandValue() == 0) {
+            System.out.println("Thank you for coming! Have a great day.");
+            return false;
+        } else {
+            DrawDealer();
+            result = calculateResult();
         }
-        DrawDealer();
-        return calculateResult();
+        if (!result) {
+            System.out.println("Thank you for coming! Have a great day.");
+        }
+        reset();
+        return result;
+    }
+    
+    /**
+     * Method to reset the blackjack table.
+     */
+    public void reset() {
+        deck = new Deck();
+        userHand = new BlackjackHand();
+        dealerHand = new BlackjackHand();
     }
 }
